a3c710aa70912c7a5321eaceb3dde06d855af034
PaT1Wat <65021026@up.ac.th>
2025-03-18 20:31:33 +0700

    update

--- frontend/App.js
+++ frontend/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import PrivateRoute from './components/routing/PrivateRoute';
-import AdminRoute from './components/routing/AdminRoute';
+import PrivateRoute from './src/components/routing/PrivateRoute';
+import AdminRoute from './src/components/routing/AdminRoute';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
+import Login from './src/components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
 import ContentList from './components/content/ContentList';
-import ContentForm from './components/content/ContentForm';
+import ContentForm from './src/components/content/ContentForm';
 import ContentEdit from './components/content/ContentEdit';
 import MediaList from './components/media/MediaList';
-import UserManagement from './components/admin/UserManagement';
+import UserManagement from './src/components/admin/UserManagement';
 import NotFound from './components/layout/NotFound';
 
 const App = () => {

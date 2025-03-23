cd65b51448a83f254863872b789db572fbcc8a50
PaT1Wat <65021026@up.ac.th>
2025-03-23 19:12:37 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    port: 5173, // พอร์ตสำหรับเซิร์ฟเวอร์ Vite
+    port: 5001, // พอร์ตสำหรับเซิร์ฟเวอร์ Vite
     proxy: {
       '/api': {
         target: 'http://localhost:5001', // ชี้ไปที่ Backend (Express)
